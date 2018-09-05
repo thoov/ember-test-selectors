8fd6a19073ad85a9dc7c32c8603033b2e6b51e6f
Tobias Bieniek <tobias.bieniek@gmail.com>
2018-09-05 09:12:57 +0200

    Update ESLint configuration

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,4 +4,47 @@ module.exports = {
     'simplabs',
     'simplabs/plugins/ember',
   ],
+  overrides: [
+    // node files
+    {
+      files: [
+        '.template-lintrc.js',
+        'ember-cli-build.js',
+        'index.js',
+        'testem.js',
+        'blueprints/*/index.js',
+        'config/**/*.js',
+        'tests/dummy/config/**/*.js',
+      ],
+      excludedFiles: [
+        'addon/**',
+        'addon-test-support/**',
+        'app/**',
+        'tests/dummy/app/**',
+      ],
+      parserOptions: {
+        sourceType: 'script',
+        ecmaVersion: 2015,
+      },
+      env: {
+        browser: false,
+        node: true,
+      },
+    },
+    // node test files
+    {
+      files: [
+        'node-tests/*.js',
+      ],
+      parserOptions: {
+        sourceType: 'script',
+        ecmaVersion: 2015
+      },
+      env: {
+        browser: false,
+        mocha: true,
+        node: true,
+      },
+    },
+  ],
 };
