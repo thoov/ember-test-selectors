a591926e4f62657d4b157e26ceee70564120134b
Tobias Bieniek <tobias@bieniek.cloud>
2021-05-06 16:21:02 +0200

    Inline `stripTestSelectors()` function
    It's no longer worth it for just two lines of code...

--- strip-test-selectors.js
+++ strip-test-selectors.js
@@ -8,11 +8,6 @@ function isTestSelector(attribute) {
   return TEST_SELECTOR_PREFIX.test(attribute);
 }
 
-function stripTestSelectors(node) {
-  node.params = node.params.filter(param => !isTestSelector(param.original));
-  node.hash.pairs = node.hash.pairs.filter(pair => !isTestSelector(pair.key));
-}
-
 function transform() {
   return {
     name: 'strip-test-selectors',
@@ -23,11 +18,13 @@ function transform() {
       },
 
       MustacheStatement(node) {
-        stripTestSelectors(node);
+        node.params = node.params.filter(param => !isTestSelector(param.original));
+        node.hash.pairs = node.hash.pairs.filter(pair => !isTestSelector(pair.key));
       },
 
       BlockStatement(node) {
-        stripTestSelectors(node);
+        node.params = node.params.filter(param => !isTestSelector(param.original));
+        node.hash.pairs = node.hash.pairs.filter(pair => !isTestSelector(pair.key));
       },
     }
   };
