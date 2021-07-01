159abac169e97c71508548d88ab6614ee8d8a435
Tobias Bieniek <tobias@bieniek.cloud>
2021-07-01 18:04:59 +0200

    Deprecate ember-cli-babel v5.x support

--- index.js
+++ index.js
@@ -63,6 +63,9 @@ module.exports = {
         appOrParent.options.babel = appOrParent.options.babel || {};
         appOrParent.options.babel.plugins = appOrParent.options.babel.plugins || [];
         appOrParent.options.babel.plugins.push(require('./strip-data-test-properties-plugin'));
+
+        this.ui.writeWarnLine('[ember-test-selectors] DEPRECATION: Using ember-cli-babel v5 with ember-test-selectors ' +
+          'is deprecated. Please make sure to update your ember-cli-babel version.');
       } else if (checker.satisfies('^6.0.0-beta.1') || checker.satisfies('^7.0.0')) {
         appOrParent.options.babel6 = appOrParent.options.babel6 || {};
         appOrParent.options.babel6.plugins = appOrParent.options.babel6.plugins || [];
